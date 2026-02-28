b8a80410f951ab8938305a8971be8140d0a79a8d
Deepak <102180983+karkideepak@users.noreply.github.com>
2026-02-28 10:47:46 -0600

    Update enumerateDomain.ps1

--- enumerateDomain.ps1
+++ enumerateDomain.ps1
@@ -3,3 +3,7 @@ Import-Module .\PowerView.ps1
 Get-NetDomain
 Get-NetCurrentUser
 Get-DomainUser -Properties samaccountname, spn | ?{$_.spn} #key
+
+svc_sql (SPN: MSSQLSvc/sqlserver.corp.local)
+svc_iis (SPN: HTTP/web.corp.local)
+svc_backup (SPN: backup/something)
